64a7019c5f84e923175cb1bf2720d9c03372fa4e
George Gumede <gumedegeorge6@gmail.com>
2023-04-10 12:53:23 +0200

    add files

--- models/base_model.py
+++ models/base_model.py
@@ -16,7 +16,7 @@ class BaseModel:
         created_at (datetime): The current datatime
         updated_at (datetime): The current dattime updated
         """
-        self.id = str(uuid.uuid4)
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
     
